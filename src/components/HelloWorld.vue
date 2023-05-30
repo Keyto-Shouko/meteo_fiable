39937fa57c85b3fa327eab77608e81f7d2c1d676
Keyto-Shouko <balvesca@gmail.com>
2023-05-30 13:02:50 +0200

    data are fetched

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,54 @@
 <script setup>
 </script>
 
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      weatherData: [],
+      timeOfRequest: Date()
+    }
+  },
+  methods: {
+    async logJSONData() {
+      const response = await fetch("https://www.infoclimat.fr/public-api/gfs/json?_ll=45.8898726,6.0873035&_auth=U0kEEwd5UnAEKVBnUiQCKwdvUGVbLVdwUy8HZA1oBHkBalc2VTUDZV4wUi8CLQo8AC0HZA02ATFTOFYuCXtTMlM5BGgHbFI1BGtQNVJ9AikHKVAxW3tXcFM5B2INfgRmAWpXN1UoA2BeMFIyAiwKNgAsB3gNMwE%2FUzhWNAlgUzNTNQRmB2FSNgR0UC1SZAIyBzNQNVs2V2pTMAdoDTQENQFhVzNVYwNkXi9SMAI6CjcANwdvDTEBPlM1Vi4Je1NJU0MEfQckUnIEPlB0Un8CYwdqUGQ%3D&_c=b0f64847b6201594895aedeb4e817ac1");
+      const jsonData = await response.json();
+      const forecast = Object.entries(jsonData).slice(5);
+      for (const [key, value] of forecast) {
+        console.log(key, value.temperature.sol);
+        let tempsPerDay = {
+          date: key,
+          temperature: value.temperature.sol - 273.15
+        }
+        this.weatherData.push(tempsPerDay);
+      }
+    }
+  },
+  mounted() {
+    this.logJSONData();
+  }
+}
+
+
+
+</script>
+
 <template>
-  <iframe seamless width="888" height="336" frameborder="0"
-    src="https://www.infoclimat.fr/public-api/mixed/iframeSLIDE?_ll=48.85341,2.3488&_inc=WyJQYXJpcyIsIjQyIiwiMjk4ODUwNyIsIkZSIl0=&_auth=U0kEEwd5UnAEKVBnUiQCKwdvUGVbLVdwUy8HZA1oBHkBalc2VTUDZV4wUi8CLQo8AC0HZA02ATFTOFYuCXtTMlM5BGgHbFI1BGtQNVJ9AikHKVAxW3tXcFM5B2INfgRmAWpXN1UoA2BeMFIyAiwKNgAsB3gNMwE%2FUzhWNAlgUzNTNQRmB2FSNgR0UC1SZAIyBzNQNVs2V2pTMAdoDTQENQFhVzNVYwNkXi9SMAI6CjcANwdvDTEBPlM1Vi4Je1NJU0MEfQckUnIEPlB0Un8CYwdqUGQ%3D&_c=b0f64847b6201594895aedeb4e817ac1">
-  </iframe>
+  <div>
+
+    <h1>Weather</h1>
+    <p>
+      Those data where fetched at : {{ timeOfRequest }}
+    </p>
+    <li v-for="temp in weatherData">
+      {{ temp }}
+    </li>
+  </div>
 </template>
 
 
