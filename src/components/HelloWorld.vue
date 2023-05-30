fcf132ec5c76a8c2e47a1891543e4ef135920b73
Keyto-Shouko <balvesca@gmail.com>
2023-05-30 10:54:05 +0200

    testing a cool little API

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,10 @@
 </script>
 
 <template>
-  TEST
+  <iframe seamless width="888" height="336" frameborder="0"
+    src="http://www.infoclimat.fr/public-api/gfs/json?_ll=48.85341,2.3488&_auth=U0kEEwd5UnAEKVBnUiQCKwdvUGVbLVdwUy8HZA1oBHkBalc2VTUDZV4wUi8CLQo8AC0HZA02ATFTOFYuCXtTMlM5BGgHbFI1BGtQNVJ9AikHKVAxW3tXcFM5B2INfgRmAWpXN1UoA2BeMFIyAiwKNgAsB3gNMwE%2FUzhWNAlgUzNTNQRmB2FSNgR0UC1SZAIyBzNQNVs2V2pTMAdoDTQENQFhVzNVYwNkXi9SMAI6CjcANwdvDTEBPlM1Vi4Je1NJU0MEfQckUnIEPlB0Un8CYwdqUGQ%3D&_c=b0f64847b6201594895aedeb4e817ac1">
+  </iframe>
 </template>
 
-<style scoped>
-</style>
+
+<style scoped></style>
